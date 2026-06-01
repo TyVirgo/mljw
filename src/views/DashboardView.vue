cccca7f457a6364ec9b1c2de3c36a42344b9c080
py <694119858@qq.com>
2026-06-01 11:53:43 +0800

    测试1153

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -42,7 +42,7 @@ const quickAccess = [
     icon: 'grid',
   },
   {
-    title: 'University Info',
+    title: 'University Info1',
     description: 'Manage school and portal branding settings.',
     pageId: 'university-info',
     iconBg: '#fce7f3',
